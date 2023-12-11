f2eb63914678ff6a6a5aa608887f9cb0fa4e37a6
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2023-12-11 01:25:58 -0300

    Melhorias

--- script.js
+++ script.js
@@ -81,9 +81,8 @@ function nextPage() {
    }
 }
 
-// Função para ir para a página anterior
 function prevPage() {
-   // Atualiza a página atual se não estiver na primeira página
+   
    if (currentPage > 1) {
       currentPage--;
       displayItems();
@@ -93,24 +92,21 @@ function prevPage() {
 function filtrarPersonagens() {
     // Obtém o valor digitado no input
     var nomePersonagem = document.getElementById("nomePersonagem").value.toLowerCase();
-    console.log("Texto digitado:", nomePersonagem);
 
-    // Obtém todas as linhas da tabela, exceto o cabeçalho
+    
     var linhas = document.querySelectorAll("#characterTable tbody tr");
 
     // Verifica se o campo de busca está vazio
     if (nomePersonagem === "") {
-        // Se estiver vazio, exibe todas as linhas paginadas
+        
         displayItems();
     } else {
-        // Se não estiver vazio, filtra as linhas com base no texto digitado
+        
         for (var i = 0; i < linhas.length; i++) {
             var nomePersonagemNaTabela = linhas[i].querySelector(".nome-heroi");
             if (nomePersonagemNaTabela) {
                 nomePersonagemNaTabela = nomePersonagemNaTabela.textContent.toLowerCase();
-                console.log("Nome na tabela:", nomePersonagemNaTabela);
 
-                // Se o nome do personagem na tabela contiver o texto digitado, exibe a linha; caso contrário, oculta
                 if (nomePersonagemNaTabela.includes(nomePersonagem)) {
                     linhas[i].style.display = "";
                 } else {
@@ -123,7 +119,6 @@ function filtrarPersonagens() {
 
 //Redirecionar para a página de detalhes do personagem
 function redirectToDetails(characterId) {
-    // Redireciona para a página de detalhes com o ID do personagem
     window.location.href = 'detalhes.php?id=' + characterId;
 }
 
